0ef01dcfa9247ccd6b445f3f55bf84f97f30ae15
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-05-19 15:07:49 -0700

    updated for a question and checked whether user entered text or not

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "githubUser",
+    message: "Please Enter your Github User Name",
+    validate: (githubUser) => {
+      if (githubUser) {
+        return true;
+      } else {
+        return "Please enter your github user name";
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+  });
+}
 
 // Function call to initialize app
 init();
