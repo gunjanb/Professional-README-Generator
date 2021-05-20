5e71024dbbd491de760d5c6b3b3dffa4cba684e2
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-05-19 23:40:06 -0700

    questions redefined , question corrected for punctuation marks and formatted for proper display

--- index.js
+++ index.js
@@ -6,83 +6,84 @@ const questions = [
   {
     type: "input",
     name: "githubUser",
-    message: "What is your Github User Name",
+    message: "What is your Github User name :",
     validate: (githubUser) => {
       if (githubUser) {
         return true;
       } else {
-        return "Please enter your github user name";
+        return "Please enter your github user name.";
       }
     },
   },
   {
     type: "input",
     name: "mailId",
-    message: "What is your mailID",
+    message: "What is your email address :",
     validate: (mailId) => {
       if (mailId) {
         return true;
       } else {
-        return "Please enter your MailId";
+        return "Please enter your email address.";
       }
     },
   },
   {
     type: "input",
     name: "projectTitle",
-    message: "What is the Project Name",
+    message: "What is the Project Name? :",
     validate: (value) => {
       if (value) {
         return true;
       } else {
-        return "Please enter a Project Name";
+        return "Please enter a Project Name.";
       }
     },
   },
   {
     type: "input",
     name: "description",
-    message: "Please provide a short decription of your project",
+    message: "Please provide a short decription for your project. :",
     validate: (description) => {
       if (description) {
         return true;
       } else {
-        return "Please enter description";
+        return "Please enter description.";
       }
     },
   },
   {
     type: "confirm",
     name: "tableContents",
-    message: "Would you like to add table of contents in your README.md file",
+    message:
+      "Would you like to add table of contents in your README.md file ? :",
     default: false,
   },
   {
     type: "input",
     name: "installationInfo",
     message:
-      "What are the steps required to install your project?(please seperate the steps by ',')",
+      "What are the steps required to install your project? (please seperate the steps by ',') :",
     validate: (installationInfo) => {
       if (installationInfo) {
         return true;
-      } else return "Please enter installation steps";
+      } else return "Please enter installation steps.";
     },
   },
   {
     type: "input",
     name: "usageInfo",
     message:
-      "Please provide instructions and examples for use.(please seperate the instructions by ',')",
+      "Please provide instructions and examples for use.(please seperate the instructions by ',') :",
     validate: (usageInfo) => {
       if (usageInfo) {
         return true;
-      } else return "Please enter usage steps";
+      } else return "Please enter usage steps.";
     },
   },
   {
     type: "list",
     name: "licenseInfo",
-    message: "choose a license for your project",
+    message: "Choose a license for your project. :",
     choices: ["Apache 2", "BSD 3", "BSD 2", "MIT", "MPL 2", "None"],
   },
   {
@@ -95,11 +96,11 @@ const questions = [
     type: "input",
     name: "contributorInfo",
     message:
-      "Please enter contributors information.(Please seperate the contributors name by ',')",
+      "Please enter contributors information.(Please seperate the contributors name by ',') :",
     validate: (contributorInfo) => {
       if (contributorInfo) {
         return true;
-      } else return "Please enter contributor Information";
+      } else return "Please enter contributor Information.";
     },
     when: (answer) => {
       //   console.log("answer is  ", answer);
@@ -113,17 +114,18 @@ const questions = [
   {
     type: "confirm",
     name: "addingTestIsTrue",
-    message: "Would you like to add Tests?",
+    message: "Would you like to add Tests for your project? :",
     default: false,
   },
   {
     type: "input",
     name: "testInfo",
-    message: "Please enter test information.(Please seperate the tests by ',')",
+    message:
+      "Please enter test information.(Please seperate the tests by ',') :",
     validate: (testInfo) => {
       if (testInfo) {
         return true;
-      } else return "Please enter test Information";
+      } else return "Please enter test Information.";
     },
     when: (answer) => {
       //   console.log("answer is  ", answer);
@@ -137,18 +139,18 @@ const questions = [
   {
     type: "confirm",
     name: "technologyIsTrue",
-    message: "Would you like to add Technology stack used?",
+    message: "Would you like to add Technology stack used? :",
     default: false,
   },
   {
     type: "input",
     name: "technologyInfo",
     message:
-      "Please enter Technology Stack used.Please seperate the technologies by ',')",
+      "Please enter Technology Stack used.(Please seperate the technologies by ',') :",
     validate: (technologyInfo) => {
       if (technologyInfo) {
         return true;
-      } else return "Please enter technology used";
+      } else return "Please enter technology used.";
     },
     when: (answer) => {
       //   console.log("answer is  ", answer);
@@ -163,7 +165,6 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((response) => {
