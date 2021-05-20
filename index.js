36893b208804be3e0598bc4937848ccc7bb01134
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-05-19 22:58:29 -0700

    updated for a question to be asked if needed

--- index.js
+++ index.js
@@ -45,6 +45,59 @@ const questions = [
     message: "Would you like to add table of contents in your README.md file",
     default: false,
   },
+  {
+    type: "input",
+    name: "installationInfo",
+    message:
+      "What are the steps required to install your project?(please seperate the steps by ',')",
+    validate: (installationInfo) => {
+      if (installationInfo) {
+        return true;
+      } else return "Please enter installation steps";
+    },
+  },
+  {
+    type: "input",
+    name: "usageInfo",
+    message:
+      "Please provide instructions and examples for use.(please seperate the instructions by ',')",
+    validate: (usageInfo) => {
+      if (usageInfo) {
+        return true;
+      } else return "Please enter usage steps";
+    },
+  },
+  {
+    type: "list",
+    name: "licenseInfo",
+    message: "choose a license for your project",
+    choices: ["Apache 2", "BSD 3", "BSD 2", "MIT", "MPL 2", "None"],
+  },
+  {
+    type: "confirm",
+    name: "contributorsIstrue",
+    message: "Would you like to add contributors?",
+    default: false,
+  },
+  {
+    type: "input",
+    name: "contributorInfo",
+    message:
+      "Please enter contributors information.(Please seperate the contributors name by ',')",
+    validate: (contributorInfo) => {
+      if (contributorInfo) {
+        return true;
+      } else return "Please enter contributor Information";
+    },
+    when: function (answer) {
+      //   console.log("answer is  ", answer);
+      if (answer.contributorsIstrue) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
 ];
 
 // TODO: Create a function to write README file
